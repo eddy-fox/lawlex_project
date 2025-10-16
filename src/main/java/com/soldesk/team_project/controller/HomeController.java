ec83b641a679102c8886b69379854b824f5ee9d3
dbfl118 <dbfl118@gmail.com>
2025-10-16 15:34:20 +0900

    index style 정리전

--- src/main/java/com/soldesk/team_project/controller/HomeController.java
+++ src/main/java/com/soldesk/team_project/controller/HomeController.java
@@ -3,12 +3,17 @@ package com.soldesk.team_project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
     
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+    @GetMapping("/aAll")
+    public String aAll() {
+        return "aAll";
     }
     
 }
