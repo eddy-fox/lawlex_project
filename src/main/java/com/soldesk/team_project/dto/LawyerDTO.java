0fd3a321905ae161b60866b731ce4b7e51a2c080
jym8905 <jym8905@gmail.com>
2025-10-19 19:38:26 +0900

    last week

---
+++ src/main/java/com/soldesk/team_project/dto/LawyerDTO.java
@@ -0,0 +1,24 @@
+package com.soldesk.team_project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LawyerDTO {
+
+    private Integer lawyerIdx;
+    private String lawyerId;
+    private String lawyerPass;
+    private String lawyerPass2;
+    private String lawyerName;
+    private String lawyerIdnum;
+    private String lawyerEmail;
+    private String lawyerPhone;
+    private String lawyerAgree;
+    private String lawyerNickname;
+    private Integer interestIdx;
+
+    private String interestName;
+
+}
