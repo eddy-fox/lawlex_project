6c77ab1d7da930c790d8407bd1f4c98142e9b725
YR <dbfl118@gmail.com>
2025-10-22 10:10:21 +0900

    광고관리리스트 adList.html

--- src/main/java/com/soldesk/team_project/controller/TemporaryController.java
+++ src/main/java/com/soldesk/team_project/controller/TemporaryController.java
@@ -16,6 +16,10 @@ import org.springframework.web.server.ResponseStatusException;
 @Controller
 public class TemporaryController {
 
+    @GetMapping("/all/admin/adList")
+    public String adList(){
+        return "/all/admin/adList";
+    }
     @GetMapping("/all/member/point")
     public String point(){
         return "/all/member/point";
