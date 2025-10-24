a69e0582a11e8250b93843f2d770055c637995cb
jym8905 <jym8905@gmail.com>
2025-10-24 10:10:22 +0900

    merge 전 저장

---
+++ src/main/java/com/soldesk/team_project/config/TossProperties.java
@@ -0,0 +1,16 @@
+package com.soldesk.team_project.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TossProperties {
+
+    @Value("${toss.secret-key}")
+    private String secretKey;
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+}
