6a98446b251d0e79a20de555c43f9b64d72115d9
YR <dbfl118@gmail.com>
2025-10-27 17:45:12 +0900

    member 폴더 10-27 학원 끝

--- src/main/java/com/soldesk/team_project/controller/TemporaryController.java
+++ src/main/java/com/soldesk/team_project/controller/TemporaryController.java
@@ -80,6 +80,14 @@ public class TemporaryController {
     public String lJoin(){
         return "/all/member/ljoin";
     }
+    @GetMapping("/all/member/gInfo")
+    public String gInfo(){
+        return "/all/member/gInfo";
+    }
+    @GetMapping("/all/member/lInfo")
+    public String lInfo(){
+        return "/all/member/lInfo";
+    }
             // 간단 캐시: "aAll" -> "member/aAll" 처럼 저장
     private static final ConcurrentHashMap<String, String> VIEW_CACHE = new ConcurrentHashMap<>();
 
