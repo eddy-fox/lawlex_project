21ab0dd2c8bcb2b71248aba4808ba4efbf5fb5d8
YR <dbfl118@gmail.com>
2025-10-31 15:05:52 +0900

    10-31 book

--- src/main/java/com/soldesk/team_project/controller/TemporaryController.java
+++ src/main/java/com/soldesk/team_project/controller/TemporaryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 @Controller
 public class TemporaryController {
 
-    
+    // asdf
     @GetMapping("/all/chat/lawyerMain")
     public String lawyerMain(){
         return "/all/chat/lawyerMain";
