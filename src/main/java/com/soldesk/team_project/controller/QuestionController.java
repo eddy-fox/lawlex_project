720ef80ecf20718fb7fe583852a3c93c02de0092
dbfl118 <dbfl118@gmail.com>
2025-11-07 21:16:33 +0900

    11-07 학원끝

--- src/main/java/com/soldesk/team_project/controller/QuestionController.java
+++ src/main/java/com/soldesk/team_project/controller/QuestionController.java
@@ -3,11 +3,17 @@ package com.soldesk.team_project.controller;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
+import com.soldesk.team_project.dto.MemberDTO;
+import com.soldesk.team_project.dto.QuestionDTO;
 import com.soldesk.team_project.service.QuestionService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/question")
@@ -25,9 +31,16 @@ public class QuestionController {
     public String qnaList() {
         return "question/qnaList";
     }
-
+    
     @GetMapping("/qnaWrite")
-    public String qnaWrite() {
+    public String qnaWrite(@ModelAttribute("qWrite") QuestionDTO qWrite) {
+        
+        return "question/qnaWrite";
+    }
+
+    @PostMapping("/qnaWrite")
+    public String qnaWriteSubmit(@ModelAttribute("qWrite") QuestionDTO qWrite) {
+
         return "question/qnaWrite";
     }
 
